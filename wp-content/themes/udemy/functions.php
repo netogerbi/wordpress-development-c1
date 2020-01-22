ff90f71615b11dfce8633fd361b85c8e6281994f
Neto <netogerbi2@hotmail.com>
2020-01-21 22:24:42 -0300

    automatically loading a file and hooking a function in an event

--- wp-content/themes/udemy/functions.php
+++ wp-content/themes/udemy/functions.php
@@ -2,12 +2,12 @@
 // this file is optional,
 // but wordpress load this file and execute code inside
 
-echo 'HELLO WORLD';
-
 // setup
 
 // includes
+include( get_theme_file_path( '/includes/front/enqueue.php') );
 
 // hooks
+add_action( 'wp_enqueue_cripts', 'ju_enqueue' );
 
 // shortcodes
