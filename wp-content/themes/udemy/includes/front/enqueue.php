90dc25069862bf135d000925a5e8465e5d575010
netogerbi <netogerbi2@hotmail.com>
2020-01-22 13:01:58 -0300

    autoloading scripts on footer

--- wp-content/themes/udemy/includes/front/enqueue.php
+++ wp-content/themes/udemy/includes/front/enqueue.php
@@ -4,7 +4,7 @@ function ju_enqueue() {
 
     $uri        = get_theme_file_uri();
 
-    // tells the wordpress where are the assets to be loaded
+    // tells the wordpress where are the styles to be loaded
     wp_register_style( 'ju_google_fonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i' );
     wp_register_style( 'ju_bootstrap', $uri . 'assets/css/bootstrap.css' );
     wp_register_style( 'ju_style', $uri . 'assets/css/style.css');
@@ -24,4 +24,14 @@ function ju_enqueue() {
     wp_enqueue_style( 'ju_magnific' );
     wp_enqueue_style( 'ju_custom' );
 
+    // tells the wordpress where are the scripts to be loaded
+    wp_register_script( 'ju_plugins', $uri . 'assets/js/plugins.js', [], false, true ); // the last true makes load in footer
+    wp_register_script( 'ju_functions', $uri . 'assets/js/functions.js', [], false, true ); // the last true makes load in footer
+
+    wp_enqueue_script( 'jquery' ); // jquery do not need to be registered, because its already registered in wordpress
+    // wordpress has a lot of ready to enqueue scripts that is commonly used by developers
+    // never override wordpress scripts
+
+    wp_enqueue_script( 'ju_plugins' );
+    wp_enqueue_script( 'ju_functions' );
 }
