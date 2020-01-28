b35902821696851bdfa9807a713b915d17536eb4
netogerbi <netogerbi2@hotmail.com>
2020-01-28 12:50:42 -0300

    styling widgets

--- wp-content/themes/udemy/includes/widgets.php
+++ wp-content/themes/udemy/includes/widgets.php
@@ -2,8 +2,13 @@
 
 function ju_widgets() {
     register_sidebar([
-        'name'          => __( 'My first theme sidebar', 'udemy' ),
-        'id'            => 'ju_sidebar',
-        'description'   => __( 'My first theme sidebar description', 'udemy' )
+        'name'              => __( 'My first theme sidebar', 'udemy' ),
+        'id'                => 'ju_sidebar',
+        'description'       => __( 'My first theme sidebar description', 'udemy' ),
+         // %1$s %2$s are placeholder for 3rd party widgets to place
+        'before_widget'    => '<div id="%1$s" class="widget cleafix %2$s',
+        'after_widget'      => '</div>',
+        'before_title'      => '<h4>',
+        'after_title'      => '</h4>'
     ]);
 }
