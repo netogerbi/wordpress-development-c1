aefcfc425c31ed8f8d5952b61768a314e964f944
netogerbi <netogerbi2@hotmail.com>
2020-01-29 12:46:26 -0300

    looping throught the posts (THE LOOP)

--- wp-content/themes/udemy/includes/setup.php
+++ wp-content/themes/udemy/includes/setup.php
@@ -1,6 +1,8 @@
 <?php
 
 function ju_setup_theme() {
+
+    add_theme_support( 'post-thumbnails' ); // process some support in the loop
     // register a navigation menu
     // this menu must be created in apearance > customize > menu. in wp dash
     register_nav_menu( 'primary', __( 'Primary Menu', 'udemy' ) );
