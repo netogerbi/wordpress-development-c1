d0817dbb8d7d6abb5a9cf3a61ff9900e4a8d63ce
netogerbi <netogerbi2@hotmail.com>
2020-01-30 12:57:53 -0300

    creating posts pagination with wordpress

--- wp-content/themes/udemy/index.php
+++ wp-content/themes/udemy/index.php
@@ -67,12 +67,25 @@
           ============================================= -->
           <div class="row mb-3">
             <div class="col-12">
+              
+              <?php
+              
+              previous_posts_link( '&larr; Older' );
+              
+              next_posts_link( 'Newer &rarr;' );
+
+              ?>
+            
+            <!--
               <a href="#" class="btn btn-outline-secondary float-left">
-                &larr; Older
+                
               </a>
               <a href="#" class="btn btn-outline-dark float-right">
-                Newer &rarr;
+                
               </a>
+
+          -->
+
             </div>
           </div>
           <!-- .pager end -->
