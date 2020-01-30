d5ad2e5b741724f92c942c1911caf6b4330717a9
Neto <netogerbi2@hotmail.com>
2020-01-30 20:01:17 -0300

    creating first plugin - file header

---
+++ wp-content/plugins/recipe/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Recipe
+ * Plugin URI: https://wordpress.org/plugins/health-check/
+ * Description: My recipes
+ * Version: 0.1.0
+ * Author: Neto
+ * Author URI: http://health-check-team.example.com
+ * Text Domain: recipe
+ * Domain Path: /languages
+*/
