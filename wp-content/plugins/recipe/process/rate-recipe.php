3137caeee10346b7bb92418b455d5c3f6b98ae29
Neto <netogerbi2@hotmail.com>
2020-02-05 20:59:01 -0300

    handling ajax request and creating rate logic

--- wp-content/plugins/recipe/process/rate-recipe.php
+++ wp-content/plugins/recipe/process/rate-recipe.php
@@ -5,6 +5,7 @@ function r_rate_recipe(){
     global $wpdb;
 
     $output         =   [ 'status' => 1 ]; // insert failure
+
     $post_ID        =   absint( $_POST['rid'] );
     $rating         =   round( $_POST['rating'], 1 );
     $user_IP        =   $_SERVER['REMOTE_ADDR'];
@@ -29,7 +30,17 @@ function r_rate_recipe(){
         [ '%d', '%f', '%s' ] // types of data that are being inserted [digit(number), float, string]
     );
 
-    $output[ 'status' ] = 2; // insert succeeded
+    // Update Recipe Metadata
+    $recipe_data                =   get_post_meta( $post_ID, 'recipe_data', true );
+    $recipe_data[ 'rating_count' ]++;
+    $recipe_data[ 'rating' ]    =   round( $wpdb->get_var(
+        "SELECT AVG(`rating`) FROM " . $wpdb->prefix . "recipe_ratings WHERE recipe_id='" . $post_ID . "'"
+    ), 1 );
+
+    update_post_meta( $post_ID, 'recipe_data', $recipe_data );
+
+
+    $output[ 'status' ]         =   2; // insert succeeded
 
     wp_send_json( $output ); // json encode the array and kill the script - perfect for ajax request
 }
--- wp-content/plugins/recipe/process/recipe-template.php
+++ wp-content/plugins/recipe/process/recipe-template.php
@@ -4,6 +4,8 @@
         <div 
             id="recipe_rating" 
             class="rateit" 
+            READONLY_PLACEHOLDER
+            data-rateit-value="RECIPE_RATING"
             data-rateit-resetable="false" 
             data-rid="RECIPE_ID"
         ></div>
